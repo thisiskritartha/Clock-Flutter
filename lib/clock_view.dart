5fc2e1b25151eea6f5c60a37f8e976df4bfd5f45
kritartha bikram shah <thisiskritartha@gmail.com>
2023-02-07 22:36:50 +0545

    fixed the bugs and updated the responsive frontend code.

--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class ClockView extends StatefulWidget {
-  const ClockView({Key? key}) : super(key: key);
+  final double size;
+  ClockView({Key? key, required this.size}) : super(key: key);
 
   @override
   State<ClockView> createState() => _ClockViewState();
@@ -22,8 +22,8 @@ class _ClockViewState extends State<ClockView> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
-      width: 300,
+      height: widget.size,
+      width: widget.size,
       child: Transform.rotate(
         angle: -pi / 2,
         child: CustomPaint(
@@ -45,13 +45,13 @@ class ClockPainter extends CustomPainter {
     var radius = min(centerX, centerY);
 
     Paint fillBrush = Paint()..color = const Color(0xFF444947);
-    canvas.drawCircle(center, radius - 40, fillBrush);
+    canvas.drawCircle(center, radius * 0.75, fillBrush);
 
     Paint outlineBrush = Paint()
       ..color = const Color(0xFFEAECFF)
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 16.0;
-    canvas.drawCircle(center, radius - 40, outlineBrush);
+      ..strokeWidth = size.width / 20;
+    canvas.drawCircle(center, radius * 0.75, outlineBrush);
 
     Paint hrHandBrush = Paint()
       ..strokeCap = StrokeCap.round
@@ -59,11 +59,15 @@ class ClockPainter extends CustomPainter {
           const RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)])
               .createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 12;
+      ..strokeWidth = size.width / 28;
     var hrHandX = centerX +
-        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+        radius *
+            0.4 *
+            cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
     var hrHandY = centerY +
-        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+        radius *
+            0.4 *
+            sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
     canvas.drawLine(center, Offset(hrHandX, hrHandY), hrHandBrush);
 
     Paint minHandBrush = Paint()
@@ -72,9 +76,11 @@ class ClockPainter extends CustomPainter {
           const RadialGradient(colors: [Color(0xFF748EF6), Color(0xFF77DDFF)])
               .createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 8;
-    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
-    var minHandY = centerY + 80 * sin(dateTime.minute * 6 * pi / 180);
+      ..strokeWidth = size.width / 35;
+    var minHandX =
+        centerX + radius * 0.52 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY =
+        centerY + radius * 0.52 * sin(dateTime.minute * 6 * pi / 180);
     canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
 
     Paint secHandBrush = Paint()
@@ -82,8 +88,10 @@ class ClockPainter extends CustomPainter {
       ..color = Colors.orange
       ..style = PaintingStyle.stroke
       ..strokeWidth = 5;
-    var secHandX = centerX + 90 * cos(dateTime.second * 6 * pi / 180);
-    var secHandY = centerY + 90 * sin(dateTime.second * 6 * pi / 180);
+    var secHandX =
+        centerX + radius * 0.64 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY =
+        centerY + radius * 0.64 * sin(dateTime.second * 6 * pi / 180);
     canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
 
     Paint centerFillBrush = Paint()..color = const Color(0xFFEAECFF);
@@ -93,15 +101,15 @@ class ClockPainter extends CustomPainter {
       ..color = const Color(0xFFEAECFF)
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 1;
+      ..strokeWidth = size.width / 250;
     var innerCircle = radius;
-    var outerCircle = radius - 16;
+    var outerCircle = radius * 0.9;
     for (double i = 0; i < 360; i += 12) {
       var x1 = centerX + outerCircle * cos(i * pi / 180);
-      var y1 = centerX + outerCircle * sin(i * pi / 180);
+      var y1 = centerY + outerCircle * sin(i * pi / 180);
 
       var x2 = centerX + innerCircle * cos(i * pi / 180);
-      var y2 = centerX + innerCircle * sin(i * pi / 180);
+      var y2 = centerY + innerCircle * sin(i * pi / 180);
 
       canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
     }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:clock_app/clock_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,11 +12,157 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('hh:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split('.').first;
+    var offsetSign = '';
+    if (!timezoneString.startsWith('-')) offsetSign = '+';
+
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: const Color(0xFF2D2F41),
-        child: ClockView(),
+      backgroundColor: const Color(0xFF2D2F41),
+      body: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildMenuButton('Clock', 'clock_icon'),
+              buildMenuButton('Alarm', 'alarm_icon'),
+              buildMenuButton('Timer', 'timer_icon'),
+              buildMenuButton('Stopwatch', 'stopwatch_icon'),
+            ],
+          ),
+          const VerticalDivider(color: Colors.white, width: 1),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 64,
+              ),
+              color: const Color(0xFF2D2F41),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: Text(
+                      'Clock',
+                      style: TextStyle(
+                        fontFamily: 'avenir',
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          formattedTime,
+                          style: const TextStyle(
+                            fontFamily: 'avenir',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 60,
+                            letterSpacing: 2,
+                          ),
+                        ),
+                        Text(
+                          formattedDate,
+                          style: const TextStyle(
+                            fontFamily: 'avenir',
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    flex: 5,
+                    fit: FlexFit.tight,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: ClockView(
+                          size: MediaQuery.of(context).size.height / 3),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    fit: FlexFit.tight,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Timezone',
+                          style: TextStyle(
+                            fontFamily: 'avenir',
+                            color: Colors.white,
+                            fontSize: 20,
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.language,
+                              color: Colors.white,
+                              size: 26,
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              'UTC $offsetSign$timezoneString',
+                              style: const TextStyle(
+                                fontFamily: 'avenir',
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding buildMenuButton(String title, String image) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: TextButton(
+        onPressed: () {},
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/$image.png',
+              scale: 1.5,
+            ),
+            Text(
+              title,
+              style: const TextStyle(
+                fontFamily: 'avenir',
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
